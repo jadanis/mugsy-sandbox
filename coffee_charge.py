84d383dfde6ab248e5bae32fccf81cada1420612
jadanis <joshallendanis@gmail.com>
2019-01-30 09:55:25 -0500

    Update coffee_charge.py

--- coffee_charge.py
+++ coffee_charge.py
@@ -2,8 +2,6 @@ import venmo
 import os
 import mysql
 
-CHARGE_COMMAND = f'venmo charge %(user) %(amount) \"Freedom ain't free, and neither is coffee!\"'
-
 def charge_setup():
   os.system("venmo configure")
 
@@ -12,6 +10,7 @@ def charge_for_coffee(user, price):
   cur.execute("select * from mugsy.profile where userID=(%s)",user)
   r = [dict((cur.description[i][0], value)
             for i, value in enumerate(row)) for row in cur.fetchall()]
-  
-  chargeString = CHARGE_COMMAND % {"user" : user_num, "amount" : price}
-  os.system(chargeString)
+  user_num = r[0] #Need to extract number from mysql query
+  CHARGE_COMMAND = f'venmo charge {user_num} {price} \"Freedom ain\'t free, and neither is coffee!\"'
+  ret_val = os.system(CHARGE_COMMAND)
+  return ret_val
